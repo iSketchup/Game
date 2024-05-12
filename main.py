f5782853909e13717ba26436c7b77d4bf900507c
luis <kalulu@gmx.at>
2024-05-12 12:20:28 +0200

    init

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import sys
+
 import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((100, 199))
+pygame.display.set_caption('HOLADIEWALDFEE')
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+
+
+	pygame.display.update()
+	clock.tick(60)
