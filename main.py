43205a83f581d16e813e3d066633e792483a55d0
luis <kalulu@gmx.at>
2024-05-12 12:56:54 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import sys
-
-import pygame
-
+import sys, pygame
+from player import Player
 
 def main():
+
 	pygame.init()
 
 	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 	pygame.display.set_caption('HOLADIEWALDFEE')
 	clock = pygame.time.Clock()
-
 	running = True
+
+	#Player init
+	player = Player(3,3,33)
 
 	while running:
 		for event in pygame.event.get():
