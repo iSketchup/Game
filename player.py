a9763025c293f72b6bcc51e50aa4383fc4804e3e
luis <kalulu@gmx.at>
2024-05-16 22:43:40 +0200

    löschh

--- player.py
+++ player.py
@@ -1,35 +1,7 @@
 import pygame
-import sys
-
-pygame.joystick.init()
-
-joysticks = []
-for x in range(pygame.joystick.get_count()):
-    joystick = pygame.joystick.Joystick(x)
-    joysticks.append(joystick)
-
-if len(joysticks)<1:
-    print("No controllers Connected")
-    sys.exit()
-else:
-    print(f"Controllers Connected: {joysticks}")
-
-
-
-print(joysticks)
-
-
-def joysticker():
-
-    speedx = round(pygame.joystick.Joystick(0).get_axis(0))
-    speedy = round(pygame.joystick.Joystick(0).get_axis(1))
-    player.moovement(speedx, speedy)
-
-
 
 class Player():
 
-    ,
     def __init__(self):
         self.player = pygame.rect.Rect(0, 0, 100, 100)
         self.color = "pink"
