442cdab56b86d6f7ff430353ba3c2acf61df542c
louiselia <lianatter2007@gmail.com>
2024-05-17 10:55:28 +0200

    changes in player class and in main for moovement

--- main.py
+++ main.py
@@ -6,22 +6,39 @@ def main():
 	pygame.init()
 
 	screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-	pygame.display.set_caption('HOLADIEWALDFEE')
+	pygame.display.set_caption("1 vs 1")
 	clock = pygame.time.Clock()
 	running = True
 
-	#Player init
-	player = Player()
+	#cotroler zeug
+	controllers = []
+
+	# Player init
+	player = Player(3)
 
 	while running:
+
+		player.displayer(screen)
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
 
 			if event.type == pygame.JOYDEVICEADDED:
-				stick = pygame.joystick.Joystick(event.device_index)
-				sticks.append(stick)
+				controller = pygame.joystick.Joystick(event.device_index)
+				controllers.append(controller)
+
+			for controller in controllers:
+				#moovement left and right
+				if controller.get_axis(0):
+					Player.moovement()
+				# up and down
+				elif controller.get_axis(1):
+					Player.moovement()
+
+
+
 
 		pygame.display.update()
 		clock.tick(60)
--- player.py
+++ player.py
@@ -2,20 +2,27 @@ import pygame
 
 class Player():
 
-    def __init__(self):
+
+
+    def __init__(self, device_index):
         self.player = pygame.rect.Rect(0, 0, 100, 100)
         self.color = "pink"
+        self.device_index = device_index
+        self.moovex = round(pygame.joystick.Joystick(0).get_axis(0))
+        self.moovey = round(pygame.joystick.Joystick(0).get_axis(1))
+        self.speedx = 10
+        self.speedy = 10
 
-    def moovement(self, speedx, speedy):
-        self.player.move_ip(speedx, speedy)
+    def moovement(self):
+        self.player.move_ip(self.moovex, self.moovey)
 
-    def displayer(self, game_screen):
-        pygame.draw.rect(game_screen, self.color, self.player)
+    def displayer(self, screen):
+        pygame.draw.rect(screen, self.color, self.player)
 
     def colorer(self, color):
         self.color = color
 
-player = Player()
+
 
 
 
