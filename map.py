5d4b54104c3d6971c9d0300667b8dbbb8a67d5aa
louiselia <lianatter2007@gmail.com>
2024-05-21 23:09:04 +0200

    probleme mit dem laden der tilesets und der map

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 import pygame
 from pytmx.util_pygame import load_pygame
 
-tmx_data = load_pygame("C:\\Users\\liana\\Schule\\HTL\\1.Klasse 2023-2024\\Game\\Attachments\\map\\tilesets and maps\\darcos_feuer_freudenhaus.tmx")
+tmx_data = load_pygame("Attachments/map/tilesets and maps/darcos_feuer_freudenhaus.tmx")
 
 print(tmx_data)
