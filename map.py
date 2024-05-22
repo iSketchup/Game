c03638585f43ba8028d8c73a77f8b108cf3f71b8
louiselia <lianatter2007@gmail.com>
2024-05-22 08:00:04 +0200

    name change

--- map.py
+++ map.py
@@ -1,6 +1,6 @@
 import pygame
 from pytmx.util_pygame import load_pygame
 
-tmx_data = load_pygame("Attachments/map/tilesets and maps/darcos_feuer_freudenhaus.tmx")
+tmx_data = load_pygame("Attachments/map/tilesets_and_maps/darcos_feuer_freudenhaus.tmx")
 
 print(tmx_data)
