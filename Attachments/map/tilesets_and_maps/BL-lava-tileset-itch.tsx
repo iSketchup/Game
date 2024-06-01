4f68a89423e878b9050c9d26956ff82a8150b0ef
louiselia <lianatter2007@gmail.com>
2024-06-01 18:33:43 +0200

    small change

---
+++ Attachments/map/tilesets_and_maps/BL-lava-tileset-itch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="BL-lava-tileset-itch" tilewidth="24" tileheight="24" tilecount="35" columns="7">
+ <image source="BL-lava-tileset-itch.png" width="168" height="120"/>
+</tileset>
