82df7da22081654067f49265f932d09e4f616638
lia <lianatter2007@gmail.com>
2024-06-13 20:49:59 +0200

    starting out with the menu buttons

---
+++ menu.py
@@ -0,0 +1,25 @@
+import pygame
+
+def main_menu(screen):
+
+    cross = pygame.image.load("Attachments/menu/dreieck.png").convert_alpha()
+    circle = pygame.image.load("Attachments/menu/Kreis.png").convert_alpha()
+    square = pygame.image.load("Attachments/menu/viereck.png").convert_alpha()
+    triangle = pygame.image.load("Attachments/menu/dreieck.png").convert_alpha()
+    pygame.display.set_caption("Menu")
+    running = True
+    while running:
+
+        screen.fill("black")
+
+
+
+def play():
+    pass
+
+def exit():
+    pass
+
+def options():
+    pass
+
